aab8e525534cb6f125dee256f1537ed69dc3c5be
Weston Ellis <ellisweston112@gmail.com>
2021-08-05 17:31:16 -0600

    Finish en/decrypt methods

--- lib/character.rb
+++ lib/character.rb
@@ -5,5 +5,16 @@ class Character
     @set = ('a'..'z').to_a << ' '
   end
 
+  def encrypt(letter, shift)
+    make_first(letter).rotate(shift).first
+  end
+
+  def decrypt(letter, shift)
+    make_first(letter).rotate(shift * -1).first
+  end
+
+  def make_first(letter)
+    @set.rotate(@set.find_index(letter))
+  end
 
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -21,14 +21,20 @@ RSpec.describe Character do
     expect(character.encrypt(' ', 27)).to eq(' ')
   end
 
+  it 'can rotate to index position' do
+    character = Character.new
+
+    expect(character.make_first('j').first).to eq('j')
+  end
+
   it 'can decrypt' do
     character = Character.new
 
-    expect(character.decrypt('a', 1)).to eq('z')
+    expect(character.decrypt('a', 1)).to eq(' ')
     expect(character.decrypt('b', 7)).to eq('v')
     expect(character.decrypt('z', 0)).to eq('z')
-    expect(character.decrypt('a', 35)).to eq('v')
-    expect(character.decrypt('0', 27)).to eq(' ')
+    expect(character.decrypt('a', 35)).to eq('t')
+    expect(character.decrypt(' ', 27)).to eq(' ')
   end
 
 end
