4d9b0d144f08154285e38e862d6d5ddde61de4ac
Weston Ellis <ellisweston112@gmail.com>
2021-08-05 15:53:48 -0600

    Finish shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,12 +1,36 @@
-def Shift
-  attr_reader :a, :b, :c, :d
+class Shift
+  attr_reader :shifts
 
-  def initialize(key, date)
-
+  def initialize(cript_key, date)
+    @shifts= {a: 0, b: 0, c: 0, d: 0}
+    @cript_key = cript_key
+    @date = date.to_i
   end
 
+  def generate_shift_nums
+    @shifts.each do |sym, shift|
+      @shifts[sym] += process_key(sym).to_i
+      @shifts[sym] += process_offset(sym)
+      # require "pry"; binding.pry
+    end
+  end
 
+  def process_key(key)
+    return @cript_key[0..1] if key == :a
+    return @cript_key[1..2] if key == :b
+    return @cript_key[2..3] if key == :c
+    return @cript_key[3..4] if key == :d
+  end
 
+  def process_offset(key)
+    return squared_last_4[0].to_i if key == :a
+    return squared_last_4[1].to_i if key == :b
+    return squared_last_4[2].to_i if key == :c
+    return squared_last_4[3].to_i if key == :d
+  end
 
-
+  def squared_last_4
+    squared = @date * @date
+    squared.to_s[-4..-1]
+  end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -1,5 +1,5 @@
 require "simplecov"
-
+require "./lib/shift"
 
 SimpleCov.start
 RSpec.describe Shift do
@@ -7,14 +7,33 @@ RSpec.describe Shift do
   it 'exists and has attributes' do
     shift = Shift.new("02715", "040895")
 
-    expect(shift.a).to eq(0)
-    expect(shift.b).to eq(0)
-    expect(shift.c).to eq(0)
-    expect(shift.d).to eq(0)
+    expect(shift.shifts).to eq({a: 0, b: 0, c: 0, d: 0})
   end
 
+  it 'can generate generate final shift' do
+    shift = Shift.new("02715", "040895")
 
+    shift.generate_shift_nums
 
+    expect(shift.shifts).to eq({a: 3, b: 27, c: 73, d: 20})
+  end
 
+  it 'can process key' do
+    shift = Shift.new("02715", "040895")
 
+    expect(shift.process_key(:a)).to eq("02")
+    expect(shift.process_key(:b)).to eq("27")
+    expect(shift.process_key(:c)).to eq("71")
+    expect(shift.process_key(:d)).to eq("15")
+  end
+
+  it 'can process offset' do
+    shift = Shift.new("02715", "040895")
+
+    expect(shift.squared_last_4).to eq("1025")
+    expect(shift.process_offset(:a)).to eq(1)
+    expect(shift.process_offset(:b)).to eq(0)
+    expect(shift.process_offset(:c)).to eq(2)
+    expect(shift.process_offset(:d)).to eq(5)
+  end
 end
