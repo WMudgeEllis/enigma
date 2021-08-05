63c70f72f8e1b9408fbbe4817afd3c1c582e3dc3
Weston Ellis <ellisweston112@gmail.com>
2021-08-05 15:06:17 -0600

    Cleanup and add barebones methods/tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,6 @@
 require "simplecov"
 require "./lib/enigma"
+require 'date'
 
 SimpleCov.start
 RSpec.describe Enigma do
@@ -8,6 +9,7 @@ RSpec.describe Enigma do
     enigma = Enigma.new
 
     allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
+    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
 
     expect(enigma).to be_a(Enigma)
     expect(enigma.encrypt("hello world", "02715", "040895")). to eq({
@@ -15,10 +17,20 @@ RSpec.describe Enigma do
       key: "02715",
       date: "040895"
       })
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq({
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      })
   end
 
+  xit 'can generate random numbers' do
 
+  end
 
+  xit "can generate today's date" do
+    expect(date_generator).to eq("050821")
+  end
 
 
 end
