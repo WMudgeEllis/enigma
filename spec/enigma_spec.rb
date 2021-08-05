594f1592c9fdfb5a27edc55d893144b738542e10
Weston Ellis <ellisweston112@gmail.com>
2021-08-05 16:48:23 -0600

    Update test to check for optional argument

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -37,5 +37,25 @@ RSpec.describe Enigma do
     expect(enigma.date_generator).to eq("050821")
   end
 
+  it "can function without all arguments" do
+    enigma = Enigma.new
+
+    allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
+    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
+    allow(enigma).to receive(:random_generator).and_return("02715")
+    allow(enigma).to receive(:date_generator).and_return("040895")
+
+
+    expect(enigma.encrypt("hello world")). to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      })
+    expect(enigma.decrypt("keder ohulw", "02715")).to eq({
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      })
+  end
 
 end
