e3a84ae57f9884b623a279fc61994a77e90676a0
Weston Ellis <ellisweston112@gmail.com>
2021-08-06 09:56:22 -0600

    Update test and skip dummy tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,7 +5,7 @@ require 'date'
 SimpleCov.start
 RSpec.describe Enigma do
 
-  it 'exists and can do mocked/stubbed methods' do
+  xit 'exists and can do mocked/stubbed methods' do
     enigma = Enigma.new
 
     allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
@@ -18,6 +18,27 @@ RSpec.describe Enigma do
       date: "040895"
       })
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq({
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      })
+  end
+
+  xit "can function without all arguments" do
+    enigma = Enigma.new
+
+    allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
+    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
+    allow(enigma).to receive(:random_generator).and_return("02715")
+    allow(enigma).to receive(:date_generator).and_return("040895")
+
+
+    expect(enigma.encrypt("hello world")). to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      })
+    expect(enigma.decrypt("keder ohulw", "02715")).to eq({
       decryption: "hello world",
       key: "02715",
       date: "040895"
@@ -37,21 +58,16 @@ RSpec.describe Enigma do
     expect(enigma.date_generator).to eq("060821")
   end
 
-  it "can function without all arguments" do
+  it 'can encrypt and decrypt' do
     enigma = Enigma.new
 
-    allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
-    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
-    allow(enigma).to receive(:random_generator).and_return("02715")
-    allow(enigma).to receive(:date_generator).and_return("040895")
-
-
-    expect(enigma.encrypt("hello world")). to eq({
+    expect(enigma).to be_a(Enigma)
+    expect(enigma.encrypt("hello world", "02715", "040895")). to eq({
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
       })
-    expect(enigma.decrypt("keder ohulw", "02715")).to eq({
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq({
       decryption: "hello world",
       key: "02715",
       date: "040895"
