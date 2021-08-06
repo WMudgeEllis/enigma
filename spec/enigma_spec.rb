fd4143bc122e07c8fb7f9acf4132d3de6263566f
Weston Ellis <ellisweston112@gmail.com>
2021-08-06 10:24:24 -0600

    Remove dummy tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,46 +5,6 @@ require 'date'
 SimpleCov.start
 RSpec.describe Enigma do
 
-  xit 'exists and can do mocked/stubbed methods' do
-    enigma = Enigma.new
-
-    allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
-    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
-
-    expect(enigma).to be_a(Enigma)
-    expect(enigma.encrypt("hello world", "02715", "040895")). to eq({
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-      })
-    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq({
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-      })
-  end
-
-  xit "can function without all arguments" do
-    enigma = Enigma.new
-
-    allow(enigma.clas).to receive(:some_method).and_return("keder ohulw")
-    allow(enigma.other_clas).to receive(:some_other_method).and_return("hello world")
-    allow(enigma).to receive(:random_generator).and_return("02715")
-    allow(enigma).to receive(:date_generator).and_return("040895")
-
-
-    expect(enigma.encrypt("hello world")). to eq({
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-      })
-    expect(enigma.decrypt("keder ohulw", "02715")).to eq({
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-      })
-  end
-
   it 'can generate random numbers' do
     enigma = Enigma.new
 
@@ -73,5 +33,27 @@ RSpec.describe Enigma do
       date: "040895"
       })
   end
+
+  it 'can use optional arguments' do
+    enigma = Enigma.new
+
+    allow(enigma).to receive(:random_generator).and_return("02715")
+    allow(enigma).to receive(:date_generator).and_return("040895")
+
+    expect(enigma.encrypt("hello world")). to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      })
+    expect(enigma.decrypt("keder ohulw", "02715")).to eq({
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      })
+  end
+
+
+
+
 
 end
